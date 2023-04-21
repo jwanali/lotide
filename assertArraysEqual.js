373620f0d08ae09947e8ea0ccb872baa8b697b20
jwanali <ciwan_ali@hotmail.com>
2023-04-21 12:26:15 -0500

    new

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};*/
+const eqArrays = function(firstArray,secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+const assertArraysEqual= function (array1,array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`\u2705 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
