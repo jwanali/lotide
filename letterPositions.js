19c5e1594c6e06b42f13944fa0fb874af6081c48
jwanali <ciwan_ali@hotmail.com>
2023-04-25 01:20:23 -0500

    new function

---
+++ letterPositions.js
@@ -0,0 +1,52 @@
+const eqArrays = function(firstArray,secondArray) {
+  
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+
+
+const assertArraysEqual= function (array1,array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`\u2705 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+
+
+const letterPositions = function(sentence) {
+
+  const result = {};
+ 
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) !== ' ') {
+      
+    
+    
+      if (result[sentence.charAt(i)]) {
+        result[sentence.charAt(i)].push(i);
+      } else {
+        result[sentence.charAt(i)] = [];
+        
+        result[sentence.charAt(i)][0] = i;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+ 
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+letterPositions("lighthouse in the house")
