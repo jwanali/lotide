1834e8959b7bb74004f28e7db20ba6260f524e4d
jwanali <ciwan_ali@hotmail.com>
2023-04-25 17:34:51 -0500

    new function

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, keyValue) {
+  for (let key of Object.keys(object)) {
+    
+    if (object[key] === keyValue) {
+     
+      return (key);
+    } 
+  }
+  return undefined
+
+};
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
