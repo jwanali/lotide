fb388a198108b0814c8459a13790dd9c89645f76
jwanali <ciwan_ali@hotmail.com>
2023-04-30 11:54:23 -0500

    some updates

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,59 +1,59 @@
 
-const eqArrays = function(firstArray,secondArray) {
-  
+const eqArrays = function (firstArray, secondArray) {
+
   if (firstArray.length !== secondArray.length) {
     return false;
   }
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       return false;;
-    } 
+    }
   }
   return true;
 };
 
 
 
-const eqObjects = function(object1, object2) {
- 
-  if (Object.keys(object1).length !== Object.keys(object2).length ) {
+const eqObjects = function (object1, object2) {
+
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } else {
-      for (const key in object1) {
-        if ( Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-         
-          if (!eqArrays(object1[key],object2[key])) {
-            return false;
-          }
-        } else if (object1[key] !== object2[key]) {
+    for (const key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+
+        if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
-    
-        
+      } else if (object1[key] !== object2[key]) {
+        return false;
       }
+
+
+    }
   }
-  
-  
+
+
 
   return true;
-  
+
 };
 
 
 // FUNCTION IMPLEMENTATION
-const assertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function (actual, expected) {
   // Implement me!
   const inspect = require('util').inspect; // <= add this line
   console.log(`Example label: ${inspect(actual)}`);
-  if (eqObjects(actual,expected)) {
+  if (eqObjects(actual, expected)) {
     console.log(`\u2705 \u2705 \u2705 Assertion Passed:  ${inspect(actual)} ===  ${inspect(expected)}`);
   } else {
     console.log(`\u26D4 \u26D4 \u26D4 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-  } 
-  
+  }
+
 };
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertObjectsEqual(multiColorShirtObject,anotherMultiColorShirtObject);
-assertObjectsEqual(multiColorShirtObject,longSleeveMultiColorShirtObject);
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+assertObjectsEqual(multiColorShirtObject, longSleeveMultiColorShirtObject);
--- findKey.js
+++ findKey.js
@@ -6,8 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 const findKey = function(object,callback) {
-
-  const keys =  Object.keys(object);
+  const keys = Object.keys(object);
   for (let key of keys) {
     if (callback(object[key])) {
     return key;
@@ -22,7 +21,18 @@ const data1 = {
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }
+const helper =  function (object){
+  if (object.stars === 1) {
+   
+   return true;
+ }
+};
+
 assertEqual(findKey(data1, x => x.stars === 1), 'Blue Hill');
 assertEqual(findKey(data1, x => x.stars === 3), 'Akaleri');
 assertEqual(findKey(data1, x => x.stars === 2), 'noma');
 assertEqual(findKey(data1, x => x.stars === 2), 'noma');
+
+
+
+assertEqual(findKey(data1,helper),'Blue Hill');
